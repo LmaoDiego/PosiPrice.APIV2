f5b678ac13e085f444d061701999a55589466814
Josue Huerta <gjhuerta_20@hotmail.com>
2021-06-30 16:18:11 -0500

    Update BDD Tests

---
+++ SpecFlowPosiPrice/SpecFlowPosiPrice/Feature/Login.feature
@@ -0,0 +1,16 @@
+﻿Feature: Login
+	Iniciar Sesión en la aplicación web de PosiPrice
+
+@smoke
+Scenario: As a User i want to login to PosiPrice Web Application
+	#steps
+	Given  I enter to the loginpage
+	When I enter my username <username> and password <password>
+	And I click the login  button
+	Then I should enter to the home page
+
+Examples: 
+	| Username         | Passoword   |
+	| gianlucalapadula | password    |
+	| cristhiancueva   | secret      |
+	| ricardogareca    | dtseleccion |
---
+++ SpecFlowPosiPrice/SpecFlowPosiPrice/Steps/LoginSteps.cs
@@ -0,0 +1,47 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowPosiPrice.Steps
+{
+    [Binding]
+    public sealed class LoginSteps
+    {
+        public static IWebDriver Instance { get; set; }
+        //Step definitions
+        [Given(@"I enter to the loginpage")]
+        public void GivenIEnterToTheLoginpage()
+        {
+            Instance = new ChromeDriver();
+            Instance.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            Instance.Url = "http://posiprice.com/LoginSteps.html";
+        }
+
+        [When(@"I enter my username (.*) and password (.*)")]
+        public void WhenIEnterMyUsernameAndPassword(string username, string password)
+        {
+            Instance.FindElement(By.Name("user")).Click();
+            Instance.FindElement(By.Name("user")).Clear();
+            Instance.FindElement(By.Name("user")).SendKeys(username);
+            Instance.FindElement(By.Name("password")).SendKeys(password);
+        }
+
+        [When(@"I click the login  button")]
+        public void WhenIClickTheLoginButton()
+        {
+            Instance.FindElement(By.XPath("/html/body/center/div/form/table/tbody/tr[]"));
+        }
+
+        [Then(@"I should enter to the home page")]
+        public void ThenIShouldSeeHello(string p0, Table table)
+        {
+            Instance.Quit();
+        }
+
+
+    }
+}
